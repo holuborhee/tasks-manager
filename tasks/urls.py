372adb666ae847fb23f5badc9850c116f1e506f2
olubori <daveholuborhee@gmail.com>
2024-07-11 23:29:52 +0100

    apply more tailwind styles and count total tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,5 @@ from .views import task_list_view
 
 urlpatterns = [
     path('api/', include('tasks.api_urls')),  # Include API URLs with "api" prefix
-    path('tasks/', task_list_view, name='home'),  # URL for the HTML template
+    path('', task_list_view, name='home'),  # URL for the HTML template
 ]
